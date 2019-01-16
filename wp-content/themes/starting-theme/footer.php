b1fdc85b8493684df1e9acb2ee14a8273de1224c
Gary McNally <garymcnally@garys-mbp.home>
2019-01-16 23:02:59 +0000

    updating body font, add signature, started main footer

--- wp-content/themes/starting-theme/footer.php
+++ wp-content/themes/starting-theme/footer.php
@@ -14,13 +14,22 @@
 	</div><!-- #content -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'starting-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'starting-theme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'starting-theme' ), 'starting-theme', '<a href="https://automattic.com/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer>
+		<div class="footer">
+			<?php wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+			) );
+				?>
+			<div class="vert-align">
+				<span>Maximus...crushing it</span>
+			</div>
+
+		</div>
+		<div class="signature">
+			© <?php echo date('Y'); ?> Maximus Crushing & Screening. All rights reserved. | <a href="/data-policy">Data Policy</a> | <a href="https://www.facebook.com/MaximusCandS/" target="_blank"><img class="fb" src="<?php echo get_template_directory_uri() ?>/images/facebook.svg" alt="Follow Us on Facebook"></a>
+			<a href="https://digital-only.com" target="_blank"><img class="do" src="<?php echo get_template_directory_uri() ?>/images/digital-only.svg" alt="Designed and developed by Digital Only"></a>
+		</div>
+	</footer><!-- /footer -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
